25ad9c588dcfa6b3fc7432b478d666a766865d91
Himanshu Parashar <himanshuparashar085@gmail.com>
2025-09-26 14:41:04 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,8 +17,3 @@ output "eks_cluster_name" {
 output "eks_cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
-
-variable "app_image" {
-  description = "Docker image for the application"
-  type        = string
-}
